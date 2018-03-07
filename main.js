59c2a54a65f5e796e771bd466c843c4c6a9e005d
Manish <memanishindian@gmail.com>
2018-03-07 15:23:13 +0530

    made change in loadurl where loadurl could load html file from local file

--- main.js
+++ main.js
@@ -16,7 +16,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL('https://www.facebook.com/')
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
